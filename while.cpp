f3824725a7009f1517eede7091d53a56ead7f3f6
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-06 19:27:45 +0530

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i=0;
+	while(1)
+	{
+		cout<<i<<" ";
+		i++;
+		if(i==10)
+		{
+			break;
+		}
+	}
+}
+OUTPUT:
+0 1 2 3 4 5 6 7 8 9
+  
+  
+  
+  
+  
+   
