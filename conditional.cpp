a0e6443220aff9c1dd3c611712e6b6d2eaf5b939
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-06 19:08:52 +0530

    Create conditional.cpp

---
+++ conditional.cpp
@@ -0,0 +1,72 @@
+//1.great of two nums
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a>>b;//10 20   
+	if(a>b)//10>20-->FALSE so goes to else block,if(0)->false,if(1)->true except 0 remaining nos true
+	{
+		cout<<"a is great";
+	}
+	else
+	{
+		cout<<"b is great";
+	}
+  
+  OUTPUT:
+ 10 20
+ 20 is great
+   
+//2.GREAT OF THREE NUMS
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cin>>a>>b>>c;//10 20 30
+	if(a>b && a>c)
+	{
+		cout<<"a is great";
+	}
+    else if(b>a && b>c)
+	{
+		cout<<"b is great";
+	}
+	else
+	{
+		cout<<"c is great";
+	}
+}
+OUTPUT:
+10 20 30
+c is great
+  
+//3.great of 3nums using multiple if
+//Multiple if-->more tym complexity coz it checks all if conditions even though if one if condition is true.so better to go with else if
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(a>b && a>c)
+	{
+		cout<<"a is great";
+	}
+    else if(b>c && b>a)
+	{
+		cout<<"b is great";
+	}
+	if(c>a && c>b)
+	{
+		cout<<"c is great";
+	}
+  OUTPUT:
+  10 20 30
+  c is great
+    
+ 
