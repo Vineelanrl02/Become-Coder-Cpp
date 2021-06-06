d79c520274330c5bbe1b21d52102490d88a07120
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-06 20:01:17 +0530

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int gcd;
+	int num1,num2;
+	cout<<"enter the values of num1 & num2"<<endl;
+	cin>>num1>>num2;
+	{
+		if(num2==0)
+		{
+		cout<<num1;
+		return gcd(num1,num2-num1);
+     	}
+    cout<<"gcd of the given numbers is"<<gcd(num1,num2);
+	return 0; 	
+    }	
+}
