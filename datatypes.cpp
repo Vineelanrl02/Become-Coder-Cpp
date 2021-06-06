5c78769048a6564ed51c1adbe99ae54e62781753
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-06 17:35:50 +0530

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num=10;
+float num1=5.78;
+char c='a';
+bool n=true;
+cout<<num<<" "<<num1<<" "<<c<<" "<<n;
+return 0;
+}
+OUTPUT:
+10 5.78 a 1
