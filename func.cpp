d8d43ccba84c1f8c82c6b80f0c5c5c4430f9e550
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-07 22:28:17 +0530

    Create func.cpp

---
+++ func.cpp
@@ -0,0 +1,16 @@
+//function-->used to reduce the codes
+//void:it is type of func. that returns no value but performing a task
+//performing void function
+#include<bits/stdc++.h>
+using namespace std;
+//function declaration
+void display()//here void prints the statement but doesnt return any value
+//function body
+{
+	cout<<"Hello";
+}
+int main()
+{
+	display();//function call
+	return 0;
+}
