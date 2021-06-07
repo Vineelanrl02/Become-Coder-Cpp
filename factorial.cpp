210f6d302e2aefeb54a2d8821f565608ec7f6b12
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-07 20:19:07 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int fact=1;
+	for(int i=2;i<=n;i++)
+	{
+		fact=fact*i;
+	}
+	cout<<fact;
+	return 0;
+}
+		
+OUTPUT:
+5
+120
+  
+  
