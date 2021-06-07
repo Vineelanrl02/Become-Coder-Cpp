f3f818e8f2ee57b004e0a53fe4f75c8d3440102f
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-07 22:15:00 +0530

    Create Sorting1.cpp

---
+++ Sorting1.cpp
@@ -0,0 +1,35 @@
+//method1
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	int fl=0;
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(arr[i]>arr[j])
+			{
+				fl=1;
+				break;
+			}
+		}
+	}
+	if(fl=1)
+	{
+		cout<<"not in sorted order";
+	}
+	else
+	{
+		cout<<"sorted order";
+	}
+	return 0;
+}
