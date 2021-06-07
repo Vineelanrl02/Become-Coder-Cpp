e708e2a7f6f2ce12fb623ac262cb082ba27fe5b4
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-07 22:31:51 +0530

    Create func2.cpp

---
+++ func2.cpp
@@ -0,0 +1,14 @@
+//passing of arguments 
+#include<bits/stdc++.h>
+using namespace std;
+void add(int x,int y)//arguments passed
+{
+	cout<<"x is: "<<x<<"\n";
+	cout<<"y is: "<<y<<"\n";
+	cout<<x+y;
+}
+int main()
+{
+	add(10,20);//working on the logic
+	return 0;
+}
