ce9c81c5f28a281c2dfec8d15f124e9177cca47e
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-07 22:32:53 +0530

    Create func3.cpp

---
+++ func3.cpp
@@ -0,0 +1,15 @@
+//prototype for void for adding a add function
+#include<bits/stdc++.h>
+using namespace std;
+void add(int ,int );//two int values will be recieved
+int main()
+{
+	add(10,20);
+	return 0;
+}
+void add(int x,int y)
+{
+	cout<<"x is: "<<x<<endl;
+  cout<<"y is: "<<y<<endl;
+	cout<<x+y;
+}
