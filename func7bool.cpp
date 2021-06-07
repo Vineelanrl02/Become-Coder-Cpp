c282b896267d0b4899b7a36e5a7879bbff7e9bb8
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-07 22:49:32 +0530

    Update func7bool.cpp

--- func7bool.cpp
+++ func7bool.cpp
@@ -12,3 +12,33 @@ int main()
   cout<<isEven(n);
   return 0;
 }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string str, int l, int r)
+{
+if (l>=r)
+{
+return true;
+}
+if (str[l] != str[r]) {
+return false;
+}
+return isPalindrome(str, l+ 1, r- 1);
+}
+int main()
+{
+string str;
+cout<<"Enter a string:";
+cin>>str;
+int len = str.length();
+if (isPalindrome(str, 0, len - 1)) {
+cout << "Palindrome";
+}
+else {
+cout << "Not Palindrome";
+}
+return 0;
+}
