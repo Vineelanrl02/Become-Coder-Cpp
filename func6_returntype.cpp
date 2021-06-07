87552113e2449d92b13b652ea9c5eaa042b5001c
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-06-07 22:44:01 +0530

    Create func6_returntype.cpp

---
+++ func6_returntype.cpp
@@ -0,0 +1,12 @@
+//int-return type function
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace srd;
+int main()
+{
+  int x=20,y=30;
+  int result;
+  result<<add(x,y);//printing of result
+  cout<<result;
+}
